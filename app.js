1caae89d2242963678e73e2d9d193f2a359df0df
Luis Rodriguez Manzur <88704054+luisrodmanzur@users.noreply.github.com>
2021-11-13 17:29:24 -0300

    objetos

---
+++ app.js
@@ -0,0 +1,70 @@
+class Book {
+    constructor(title, author, year, gender) {
+        this.title = title
+        this.author = author
+        this.year = year
+        this.gender = gender
+    }
+
+    getAuthor() {
+        return this.author
+    }
+
+    getGender() {
+        return this.gender
+    }
+
+    bookInfo() {
+        return `${this.title} es un libro de ${this.gender} escrito por ${this.author} en el año ${this.year}`
+    }
+}
+
+let books = []
+
+while (books.length < 3) {
+    let title = prompt('Introduzca el título del libro')
+    let author = prompt('Introduzca el autor del libro')
+    let year = prompt('Introduzca el año del libro')
+    let gender = prompt('Introduzca el género del libro').toLowerCase()
+
+    if (title != '' &&
+        author != '' &&
+        !isNaN(year) &&
+        year.length == 4 &&
+        (gender == 'aventura' || gender == 'terror' || gender == 'fantasia')) {
+
+        books.push(new Book(title, author, year, gender))
+
+    }
+}
+
+const showAllBooks = () => {
+    console.log(books);
+}
+
+const showAuthors = () => {
+    let authors = []
+
+    for (const book of books) {
+        authors.push(book.getAuthor());
+    }
+
+    console.log(authors.sort());
+}
+
+const showGender = () => {
+    const gender = prompt('Introduce el género a buscar')
+
+    for (const book of books) {
+        if (book.getGender() == gender) {
+            console.log(book.bookInfo())
+        }
+    }
+
+}
+
+//showAllBooks()
+//showAuthors()
+showGender()
+
+
